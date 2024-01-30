14a69eaca7dfb593fa20dced628d5e18d51c96ca
NCanan <nabiicanan78@gmail.com>
2024-01-30 11:54:56 -0500

    Added powerpoint instructions

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from booking_report_function import on_file_selected
 from queries import new_function
-
+import os
 
 # Completed
+
+
+def open_powerpoint():
+    powerpoint_file = r'"P:\Partnership_Python_Projects\Booking Report Program\Booking Report Program Instructions.pptx"'
+
+    try:
+        os.startfile(powerpoint_file)
+    except Exception as e:
+        print(f"Error: {e}")
+
+
 def setup_gui(main_window):
     style = ttk.Style()
     main_window.configure(bg="white")
@@ -25,6 +36,10 @@ def setup_gui(main_window):
                                   justify="center")
     description_label.pack(pady=20)
 
+    open_powerpoint_button = ttk.Button(main_window, text='Open PowerPoint Instructions',
+                                        command=open_powerpoint)
+    open_powerpoint_button.pack(pady=10)
+
     run_queries_button = ttk.Button(main_window, text="Run Booking Report Query",
                                     command=new_function, style="TButton")
     run_queries_button.pack(pady=20)
@@ -37,6 +52,6 @@ def setup_gui(main_window):
 if __name__ == "__main__":
     root = tk.Tk()
     root.title('Creation File Setup')
-    root.geometry('1000x480')
+    root.geometry('1000x580')
     setup_gui(root)
     root.mainloop()
